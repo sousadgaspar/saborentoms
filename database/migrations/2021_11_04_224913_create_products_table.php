3add07bebcabc637a64e49ac50716454b2f402b2
Sousa Gaspar <sousadgaspar@gmail.com>
2021-11-05 00:01:55 +0100

    first commit

---
+++ database/migrations/2021_11_04_224913_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('vendor')->default('não definido');
+            $table->string('brand')->default('sem marca');
+            $table->string('description')->nullable();
+            $table->double('unitPrice')->default(0);
+            $table->integer('quantity')->default(1);
+            $table->date('production')->nullable();
+            $table->date('expiration')->nullable();
+            $table->string('batch')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
